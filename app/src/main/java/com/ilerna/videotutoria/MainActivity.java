cbad9e041645327a79ae3eaa13f78c4211c025f1
Rafael Perez-Beato <rperezbeato@gmail.com>
2020-05-24 16:30:02 +0200

    Creamos los botones.

--- app/src/main/java/com/ilerna/videotutoria/MainActivity.java
+++ app/src/main/java/com/ilerna/videotutoria/MainActivity.java
@@ -3,9 +3,14 @@ package com.ilerna.videotutoria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button apareceTexto;
+    Button cambiaTexto;
 
     //El onCreate se crea automaticamente
     @Override
@@ -14,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Toast para crear textos, se mete el contexto, funcion en este caso al crearse el activity y la duración del mensaje
         Toast.makeText(this,getText(R.string.onCreate),Toast.LENGTH_SHORT).show();
+        View apareceTexto = findViewById(R.id.apareceTexto);
+        View cambiaTexto = findViewById(R.id.cambiaTexto);
+
+
+
 
     }
 
